b3928ffe8581638c0af3254429cb01651fb2b038
IsmailTheGreatest <2110049@newuzbekistanuniversity.uz>
2023-02-15 12:10:26 +0500

    final
    ismail

---
+++ LibrarianApp/src/librarianapp/LibrarianApp.java
@@ -0,0 +1,7 @@
+package librarianapp;
+
+public class LibrarianApp {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
---
+++ MemberApp/src/memberapp/MemberApp.java
@@ -0,0 +1,7 @@
+package memberapp;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
